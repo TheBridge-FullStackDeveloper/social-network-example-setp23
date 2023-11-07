00192c4462dffccf9fe00de092a694351e6bcc39
SofiaPinilla <contact@sofiapinilla.com>
2023-11-07 10:24:50 +0100

    testing implemented! !

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const { typeError } = require('./middlewares/errors');
-
+const { typeError } = require("./middlewares/errors");
+// console.log(process.env.NODE_ENV)
 const PORT = 8080;
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/users", require("./routes/users.js"));
 app.use("/posts", require("./routes/posts.js"));
-    
-app.use(typeError)
+
+app.use(typeError);
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const UserController = require("../controllers/UserController");
 const { authentication, isAdmin } = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/create", UserController.create);
-router.get("/", UserController.getAll);
+router.post("/register", UserController.create);
+router.get("/",authentication, UserController.getAll);
 router.delete("/id/:id", authentication, isAdmin, UserController.delete);
-router.put("/id/:id", UserController.update);
+router.put("/id/:id",authentication, UserController.update);
 router.post("/login", UserController.login);
 router.delete("/logout", authentication, UserController.logout);
 router.get('/confirm/:email',UserController.confirm)
